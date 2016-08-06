2e00a8dea8ee66a7e81f0e9d4df78006eb908c66
Jennifer Buehler <jenniferb@cse.unsw.edu.au>
2016-08-06 17:27:00 +0200

    Rebase from 'upstream'

--- urdf2inventor/src/Urdf2Inventor.cpp
+++ urdf2inventor/src/Urdf2Inventor.cpp
@@ -1,20 +1,34 @@
 /**
-    Copyright (C) 2016 Jennifer Buehler
+ * <ORGANIZATION> = Jennifer Buehler 
+ * <COPYRIGHT HOLDER> = Jennifer Buehler 
+ * 
+ * Copyright (c) 2016 Jennifer Buehler 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <ORGANIZATION> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ------------------------------------------------------------------------------
+ **/
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-**/
 #include <urdf2inventor/Urdf2Inventor.h>
 #include <urdf2inventor/Helpers.h>
 #include <urdf2inventor/IVHelpers.h>
@@ -61,15 +75,17 @@ bool Urdf2Inventor::joinFixedLinks(const std::string& fromLink)
     {
         startLinkName = urdf_traverser->getRootLinkName();
     }
+        
+    ROS_INFO("############### Joining fixed links");
+    ROS_INFO_STREAM("Start from root link: "<<startLinkName);
 //    ROS_INFO_STREAM("Getting as inventor starting from '"<<startLinkName<<"'");
     LinkPtr startLink = urdf_traverser->getLink(startLinkName);
     if (!startLink.get())
     {
-        ROS_ERROR_STREAM("No link named '" << startLink << "'");
+        ROS_ERROR_STREAM("No link named '" << startLinkName << "'");
         return false;
     }
 
-    ROS_INFO("############### Joining fixed links");
     if (!urdf_transform::joinFixedLinks(*urdf_traverser, startLinkName))
     {
         ROS_ERROR_STREAM("Could not join fixed links");
@@ -394,7 +410,7 @@ SoNode * Urdf2Inventor::getAsInventor(const LinkPtr& from_link, bool useScaleFac
         // ROS_WARN_STREAM("Transform joint "<<joint->name<<": "<<jointTransform);
         // ROS_INFO_STREAM("Adding sub node for "<<childLink->name);
 
-        allVisuals = urdf2inventor::addSubNode(childNode, allVisuals, jointTransform);
+        urdf2inventor::addSubNode(childNode, allVisuals, jointTransform);
     }
 
     return allVisuals;
